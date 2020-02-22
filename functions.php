654f0938e13285e7d651fc31d1152b5b821fa743
trkea <trs1141@yahoo.co.jp>
2020-02-22 20:09:06 +0900

    2：開発を始める前に

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+// This function enqueues the Normalize.css for use. The first parameter is a name for the stylesheet, the second is the URL. Here we
+// use an online version of the css file.
+//異なるブラウザ間で変わるmarginやpaddingを自動で調節してくれる
+function add_normalize_CSS() {
+    wp_enqueue_style( 'normalize-styles', "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css");
+}
+
+// Register a new sidebar simply named 'sidebar'
+//メニューリンクのウィジェットを追加できるようにする
+function add_widget_Support() {
+    register_sidebar( array(
+                    'name'          => 'Sidebar',
+                    'id'            => 'sidebar',
+                    'before_widget' => '<div>',
+                    'after_widget'  => '</div>',
+                    'before_title'  => '<h2>',
+                    'after_title'   => '</h2>',
+    ) );
+}
+// Hook the widget initiation and run our function
+add_action( 'widgets_init', 'add_Widget_Support' );
+
+// Register a new navigation menu
+//カスタムナビゲーションメニューを登録して、外観->メニュー機能を使用できるようにします
+function add_Main_Nav() {
+    register_nav_menu('header-menu',__( 'Header Menu' ));
+  }
+  // Hook to the init action hook, run our navigation menu function
+  add_action( 'init', 'add_Main_Nav' );
+
+?>
---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<?php // //テーマのどこにでもサイドバーとウィジェットを表示できるようにする?>
+<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+  <aside id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+    <?php dynamic_sidebar( 'sidebar' ); ?>
+  </aside>
+<?php endif; ?>
